a2e1bd97f0f31849c7d0fea625c2325e75975462
Danny <contact@danfoster.io>
2018-05-15 12:41:02 -0500

    Final

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,1 +1,6 @@
 // Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+const almostIncreasingSequence = sequence =>
+  sequence
+    .map((removedInt, index) => sequence.filter((int, i) => i !== index))
+    .some(singleSeq => singleSeq.every((int, i, currentArr) => i === 0 || currentArr[i - 1] < int));
