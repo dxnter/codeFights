457f9183d1ebfaa9e169ce31f99902d9d2cba33e
Danny <contact@danfoster.io>
2018-05-15 21:03:09 -0500

    Folder restructure

---
+++ Arcade/arrayPacking.js
@@ -0,0 +1,7 @@
+// https://codefights.com/arcade/code-arcade/corner-of-0s-and-1s/KeMqde6oqfF5wBXxf
+
+const arrayPacking = a => a.map(number => number.toString(2));
+
+console.log(arrayPacking([24, 85, 0]));
+// 134480385
+
